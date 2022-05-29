55e3ca44ed07590c7db9109f36fc0c726d8f1404
kyungparkmin <kyungparkmin@naver.com>
2022-05-29 14:37:23 +0900

    Update 0529 비교연산자.java

--- 2205/0529 비교연산자.java
+++ 2205/0529 비교연산자.java
@@ -3,65 +3,47 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+
+        String type;
         Scanner in = new Scanner(System.in);
-        for(int i = 1; i <= 1000; i++){
+        int i = 1;
+        while(true){
             int n1 = in.nextInt();
-            String type = in.next();
+            type = in.next();
             int n2 = in.nextInt();
 
-            if(type.equals("E")){
+            if(type.equals("E") || type.equals("e")){
                 break;
             }
             switch(type){
-                case "!=":
-                    if(n1 != n2){
-                        System.out.printf("Case %d: true", i);
-                        break;
-                    }else {
-                        System.out.printf("Case %d: false", i);
-                        break;
-                    }
-                case "==":
-                    if(n1 == n2){
-                        System.out.printf("Case %d: true", i);
-                        break;
-                    }else {
-                        System.out.printf("Case %d: false", i);
-                        break;
-                    }
-                case "<":
-                    if(n1<n2){
-                        System.out.printf("Case %d: true", i);
-                        break;
-                    }else{
-                        System.out.printf("Case %d: false", i);
-                        break;
-                    }
-                case "<=":
-                    if(n1<=n2){
-                        System.out.printf("Case %d: true", i);
-                        break;
-                    }else{
-                        System.out.printf("Case %d: false", i);
-                        break;
-                    }
-                case ">=":
-                    if(n1>=n2){
-                        System.out.printf("Case %d: true", i);
-                        break;
-                    }else{
-                        System.out.printf("Case %d: false", i);
-                        break;
-                    }
                 case ">":
-                    if(n1>n2){
-                        System.out.printf("Case %d: true", i);
-                        break;
-                    }else{
-                        System.out.printf("Case %d: false", i);
-                        break;
-                    }
+                    System.out.print("Case " + i +": ");
+                    System.out.println(n1>n2);
+                    break;
+                case "==":
+                    System.out.print("Case " + i +": ");
+                    System.out.println(n1==n2);
+                    break;
+                case "<":
+                    System.out.print("Case " + i +": ");
+                    System.out.println(n1<n2);
+                    break;
+                case "<=":
+                    System.out.print("Case " + i +": ");
+                    System.out.println(n1<=n2);
+                    break;
+                case ">=":
+                    System.out.print("Case " + i +": ");
+                    System.out.println(n1>=n2);
+                    break;
+                case "!=":
+                    System.out.print("Case " + i +": ");
+                    System.out.println(n1!=n2);
+                    break;
+                default:
+                    return;
             }
+            i++;
         }
         in.close();
     }
